d1e9b3d63c0c5085a5bd99390cb73c4db4e1b72c
akhdanpangestuajiwidodo <akhdan1303@gmail.com>
2022-12-29 16:05:41 +0700

    register bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+import 'package:coffee_api/blocs/coffe_event.dart';
+import 'package:coffee_api/blocs/coffee_bloc.dart';
+import 'package:coffee_api/repositories/coffee_remote_data_repository.dart';
 import 'package:coffee_api/screens/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,13 +15,20 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Coffee Api',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<CoffeeBloc>(
+            create: (BuildContext context) =>
+                CoffeeBloc(CoffeeRemoteDataRepository())..add(GetCoffeEvent())),
+      ],
+      child: MaterialApp(
+        title: 'Coffee Api',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        initialRoute: HomeScreen.routeName,
+        routes: _router,
       ),
-      initialRoute: HomeScreen.routeName,
-      routes: _router,
     );
   }
 
